03e7b86ec1ad093c81965ea8a7bb6074669d07cc
MuhammadSiqaf <msiqaf@gmail.com>
2022-12-09 22:25:13 +0500

    Add project files.

---
+++ Electronic-Tickets/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Electronic_Tickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
