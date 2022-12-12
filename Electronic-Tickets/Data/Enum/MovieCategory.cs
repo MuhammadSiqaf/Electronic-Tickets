b0452ebfa76e90fe88e2f78d7d0657300d0207dc
MuhammadSiqaf <msiqaf@gmail.com>
2022-12-12 22:30:09 +0500

    Add project files.

---
+++ Electronic-Tickets/Data/Enum/MovieCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Electronic_Tickets.Data
+{
+    public enum MovieCategory
+    {
+        Action=1,
+        Comedy,
+        Drama,
+        Documentry,
+        horror,
+    }
+}
